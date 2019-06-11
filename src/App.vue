87d81a797ca26857669ae314564a67a3410196a8
robertdole <drpenguin57@gmail.com>
2019-06-10 22:39:38 -0400

    cleaning up html

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/about">About</router-link> | -->
       <router-link to="/examples">Examples</router-link>
       <!-- <router-link to="/test3d">Test3d</router-link> -->
     </div>
--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="about">
-    <h1>Sample Text</h1>
+    <h1>Example Projects</h1>
+    <p>A couple of example projects created using Vue. Click the images to see more. </p>
     <div class = "row">
-        <div class="column">
+        <!-- <div class="column">
             <h3><img class="icon-image" src="../img/computerrepair.jpg"></h3>
             <h3><a href="http://www.google.com/"><img class="icon-image" src="../img/excel.jpeg"></a></h3>
             <h3><a href="localhost:8080/views/3dengine/3dmain.html"><img class="icon-image" src="../img/cubebkg02.jpeg"></a></h3>
@@ -11,12 +12,21 @@
             <h3><img class="icon-image" src="../img/computerrepair.jpg"></h3>
             <h3><img class="icon-image" src="../img/computerrepair.jpg"></h3>
             <h3><img class="icon-image" src="../img/computerrepair.jpg"></h3>
-        </div>
-        <div class="column">
+        </div> -->
+        <!-- <div class="column"> -->
             <!-- <h3><a href="localhost:8080/views/3dengine/3dmain.html">3d</a></h3> -->
             <router-link to="./examples/tableapp"><img class="icon-image" src="../img/excel.jpeg"></router-link>
+            <h3>Data Tables</h3>
+            <p>A data table app that takes a generic json string and converts it into a data table.</p>
+            <p>adding or editing cells is only visual. Data is not retained if page is reloaded. </p>
+            <br/><br/><br/>
             <router-link to="./examples/3dmain"><img class="icon-image" src="../img/ascii.jpeg"></router-link>
-            <router-link to="./examples/testcanvas"><img class="icon-image" src="../img/computerrepair.jpg"></router-link>
+            <h3>3D Renderer</h3>
+            <p>3D rendering using the 2D html 5 canvas. loads in obj files exported from blender.</p>
+            <!-- <p><a href="https://github.com/robertdole/vuesite/tree/master/src/views/3dengine/monkey2.obj">test
+               
+            </a></p> -->
+            <!-- <router-link to="./examples/testcanvas"><img class="icon-image" src="../img/computerrepair.jpg"></router-link> -->
         </div>
 
     </div>
@@ -27,15 +37,16 @@
 
 export default {
   name: 'examples',
-  
+
 
 }
 </script>
 <style>
 .row{
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 4px;
+      display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
 }
 .column {
   flex: 25%;
@@ -56,8 +67,8 @@ export default {
 }
     .icon-image {
     
-        width: 130px;
-        height: 96px;
+        width: 200px;
+        height: 160px;
 
     }
 </style>
